8b31a03124ef01c7edacf226f581c3d4c5eddc41
rawan-farakhna <rawanali1997@yahoo.com>
2025-01-06 08:34:10 +0200

    Update Skills Page

---
+++ main.js
@@ -0,0 +1,24 @@
+const projectsSection = document.querySelector('.projects-section-horizontal');
+const projects = projectsSection.innerHTML; // Get the content of the section
+
+// Clone the content to make it infinite
+projectsSection.innerHTML += projects;
+
+function animateScroll() {
+  const totalWidth = projectsSection.scrollWidth / 2; // Divide by 2 for the total scrollable width (since we're duplicating the content)
+  let currentScroll = 0;
+
+  setInterval(() => {
+    // Scroll the section
+    projectsSection.scrollLeft = currentScroll;
+    currentScroll++;
+
+    // When it reaches the end, reset to start
+    if (currentScroll >= totalWidth) {
+      currentScroll = 0;
+    }
+  }, 20); // Adjust the speed by changing the interval
+}
+
+// Start the animation
+animateScroll();
